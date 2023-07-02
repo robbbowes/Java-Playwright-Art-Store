1f36fbc70b8f171b93ac02825ce301737094ecef
Robb Bowes <robertjamesbowes@gmail.com>
2023-07-02 11:48:50 +0200

    Add NavigationBehaviour and PageElementLocator

--- src/test/java/core/utils/NavigationBehaviour.java
+++ src/test/java/core/utils/NavigationBehaviour.java
@@ -1,4 +1,62 @@
 package core.utils;
 
+import com.microsoft.playwright.Page;
+import core.config.TestContext;
+import core.records.PageRouteInfo;
+import core.records.RouteClassResult;
+import org.testng.Assert;
+import pages.abstractions.CucumberPage;
+
+import java.util.Map;
+
 public class NavigationBehaviour {
+
+    public static void navigateToPage(TestContext testContext, String pageId) {
+        final Page currentTab = testContext.getScreen().getCurrentTabInfo().currentTab();
+        final RouteClassResult result = getResult(testContext.getMappings(), pageId);
+        final PageRouteInfo routeInfo = result.routeInfo();
+        final CucumberPage currentTabClass = result.pageClass();
+
+        final String url = testContext.getBaseUrl() + routeInfo.route();
+        currentTab.navigate(url);
+
+        currentTab.waitForURL(routeInfo.regexPattern());
+        testContext.getScreen().setCurrentTabInfo(currentTab, currentTabClass);
+    }
+
+    public static void waitForCorrectPage(TestContext testContext, String pageId) {
+        final Page currentTab = testContext.getScreen().getCurrentTabInfo().currentTab();
+        final RouteClassResult result = getResult(testContext.getMappings(), pageId);
+        final PageRouteInfo routeInfo = result.routeInfo();
+        final CucumberPage currentTabClass = result.pageClass();
+
+        currentTab.waitForURL(routeInfo.regexPattern());
+        testContext.getScreen().setCurrentTabInfo(currentTab, currentTabClass);
+    }
+
+    public static CucumberPage getCurrentTabClass(Map<CucumberPage, PageRouteInfo> routeMappings, Page currentTab) {
+        final String url = currentTab.url();
+
+        final CucumberPage cucumberPage = routeMappings.entrySet().stream()
+                .filter(entry -> url.matches(entry.getValue().regexPattern().pattern()))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+
+        Assert.assertNotNull(cucumberPage, String.format("Page class not found for url: '%s'", url));
+        return cucumberPage;
+    }
+
+    private static RouteClassResult getResult(Map<CucumberPage, PageRouteInfo> routeMappings, String pageId) {
+        final RouteClassResult routeClassResult = routeMappings.keySet().stream()
+                .filter(pageClass -> pageId.equals(pageClass.getClass().getSimpleName()))
+                .map(pageClass -> new RouteClassResult(routeMappings.get(pageClass), pageClass))
+                .findFirst()
+                .orElse(null);
+
+        Assert.assertNotNull(routeClassResult, String.format("Route info and class not found for page with id: '%s'", pageId));
+        return routeClassResult;
+
+    }
+
 }
--- src/test/java/core/utils/PageElementLocator.java
+++ src/test/java/core/utils/PageElementLocator.java
@@ -1,4 +1,34 @@
 package core.utils;
 
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import core.config.Screen;
+import core.config.TestContext;
+import org.testng.Assert;
+
+import java.util.Map;
+
 public class PageElementLocator {
+
+    public static Locator getLocator(TestContext testContext, String locatorKey) {
+        final Screen screen = testContext.getScreen();
+        final Locator elementLocator = PageElementLocator.queryClassForLocator(screen, locatorKey);
+
+        final String errorMessage = String.format("No locator with key '%s' found in the mappings of the '%s' class.",
+                locatorKey, testContext.getScreen().getCurrentTabInfo().currentTabClass().getClass().getSimpleName());
+        Assert.assertNotNull(elementLocator, errorMessage);
+
+        return elementLocator;
+    }
+
+    private static Locator queryClassForLocator(Screen screen, String locatorKey) {
+        final Page page = screen.getCurrentTabInfo().currentTab();
+        final Map<String, Locator> locators = screen.getCurrentTabInfo()
+                .currentTabClass()
+                .getLocators(page);
+
+        Assert.assertNotNull(locators);
+        return locators.get(locatorKey);
+    }
+
 }
